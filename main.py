876602b686107142949a3fb116dfb3f74e566a47
MoodzUnl <rayman30@live.com.mx>
2023-11-29 18:41:43 -0600

    visualizations update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import cross_val_score
 from hyperopt import hp, fmin, tpe, Trials, STATUS_OK
 import time
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Cargar el conjunto de datos
 data = load_iris()
@@ -43,7 +44,8 @@ time_optimization = end_time_opt - start_time_opt
 # Entrenar y evaluar el modelo con hiperparámetros optimizados
 best_params['n_estimators'] = int(best_params['n_estimators'])
 best_params['max_depth'] = int(best_params['max_depth'])
-best_params['min_samples_split'] = int(best_params['min_samples_split'])
+best_params['min_samples_split'] = max(2, int(best_params['min_samples_split']))
+
 model_optimized = RandomForestClassifier(**best_params, random_state=0)
 start_time_opt_model = time.time()
 scores_optimized = cross_val_score(model_optimized, X, y, cv=5)
@@ -57,3 +59,46 @@ print("Precisión media (default):", mean_score_default)
 print("Tiempo de optimización:", time_optimization)
 print("Tiempo de entrenamiento (optimizado):", time_opt_model)
 print("Precisión media (optimizada):", mean_score_optimized)
+
+# Gráfico de barras para comparar
+labels = ['Default', 'Optimized']
+time_values = [time_default, time_opt_model]
+score_values = [mean_score_default, mean_score_optimized]
+
+x = np.arange(len(labels))  # Ubicaciones de las etiquetas
+width = 0.35  # Ancho de las barras
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width/2, time_values, width, label='Tiempo')
+rects2 = ax.bar(x + width/2, score_values, width, label='Precisión')
+
+# Añadir texto para etiquetas, título y etiquetas personalizadas de los ejes
+ax.set_ylabel('Valores')
+ax.set_title('Comparación entre modelo por defecto y optimizado')
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.legend()
+
+ax.bar_label(rects1, padding=3)
+ax.bar_label(rects2, padding=3)
+
+fig.tight_layout()
+
+# Gráficas individuales para cada modelo
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.plot(scores_default, label='Default', marker='o')
+plt.title('Rendimiento del Modelo Default')
+plt.xlabel('Número de Validación')
+plt.ylabel('Precisión')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(scores_optimized, label='Optimized', marker='o')
+plt.title('Rendimiento del Modelo Optimizado')
+plt.xlabel('Número de Validación')
+plt.ylabel('Precisión')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
